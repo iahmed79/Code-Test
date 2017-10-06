42ba581361dab102c8035d8ea66a76a5708c1e95
Iftekhar Ahmed <Iftekhar.Ahmed@asos.com>
2017-10-06 09:02:01 +0100

    Add project files.

---
+++ App.Tests/CustomerServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace App.Tests
+{
+    [TestFixture]
+    public class CustomerServiceTests
+    {
+        [Test]
+        public void GivenFirstNameIsEmptyReturnsFalse()
+        {
+            Mock<ICompanyRepository> companyRepository = new Mock<ICompanyRepository>();
+
+            var customerService = new CustomerService(companyRepository.Object);
+            var result = customerService.AddCustomer(string.Empty, "surname", "email", DateTime.Now, 1);
+
+            Assert.False(result);
+        }
+    }
+}
