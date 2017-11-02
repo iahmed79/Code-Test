2cac6eeeaf917d0aa6d907637de8c7537c028c95
Iftekhar Ahmed <Iftekhar.Ahmed@asos.com>
2017-11-02 09:30:15 +0000

    Added verification to check whether AddCustomer was invoked correctly

--- App.Tests/CustomerServiceTests.cs
+++ App.Tests/CustomerServiceTests.cs
@@ -20,7 +20,7 @@ namespace App.Tests
         }
 
         [Test]
-        public void GivenAValidCustomerWhenAddCustomerThenCompanyRepositoryIsCalled()
+        public void GivenAValidVeryImportantCustomerWhenAddCustomerThenIsSaved()
         {
             var company = new Company();
             company.Name = "VeryImportantClient";
@@ -31,7 +31,28 @@ namespace App.Tests
             var customerService = new CustomerService(companyRepository.Object, customerDataAccessWrapper.Object);
             customerService.AddCustomer("firstname", "surname", "email@email.com", DateTime.Now.AddYears(-25), 1);
 
-            
+            var expected = new Customer
+            {
+                Company = new Company(),
+                CreditLimit = 0,
+                DateOfBirth = DateTime.Now.AddYears(-25),
+                EmailAddress = "email@email.com",
+                Firstname = "firstname",
+                Surname = "surname",
+                HasCreditLimit = false,
+                Id = 7
+            };
+
+            customerDataAccessWrapper.Verify(x => x.AddCustomer(It.Is<Customer>(a => IsValidCustomer(a, expected))),
+                Times.Once());
+        }
+
+        private bool IsValidCustomer(Customer actual, Customer expected)
+        {
+            Assert.That(actual.Surname, Is.EqualTo(expected.Surname));
+            Assert.That(actual.Firstname, Is.EqualTo(expected.Firstname));
+
+            return true;
         }
     }
 }
